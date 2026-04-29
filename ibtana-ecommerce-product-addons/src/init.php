6668c1f1327b6cd78282641da1f3cfd199eddefb
Sean Murphy <sean@iamseanmurphy.com>
2026-04-29 07:05:40 +0000

    0.4.7.7

--- ibtana-ecommerce-product-addons/src/init.php
+++ ibtana-ecommerce-product-addons/src/init.php
@@ -53,12 +53,13 @@ function ibtana_ecommerce_product_addons_iepa_block_assets() { // phpcs:ignore
 		null // filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.editor.build.css' ) // Version: File modification time.
 	);
 
-	wp_enqueue_script( 'iepa-slick-js', plugins_url( '/dist/js/slick.min.js', dirname( __FILE__ ) ), array( 'jquery' ), IEPA_VERSION, false );
-	wp_enqueue_script( 'iepa-fancybox-js', plugins_url( '/dist/js/jquery.fancybox.js', dirname( __FILE__ ) ), array( 'jquery' ), IEPA_VERSION, true );
-	wp_enqueue_script( 'iepa-zoom-js', plugins_url( '/dist/js/jquery.zoom.min.js', dirname( __FILE__ ) ), array( 'jquery' ), IEPA_VERSION, true );
-	wp_enqueue_style( 'iepa-fancybox-css', plugins_url( '/dist/css/fancybox.css', dirname( __FILE__ ) ), [], IEPA_VERSION );
-	wp_enqueue_style( 'iepa-front-css', plugins_url( '/dist/css/gallery-slider.css', dirname( __FILE__ ) ), array( 'dashicons' ), IEPA_VERSION );
-	wp_enqueue_script( 'iepa-front-js', plugins_url( '/dist/js/gallery-slider.js', dirname( __FILE__ ) ), array( 'jquery' ), IEPA_VERSION, true );
+	// Register front-end assets and enqueue them conditionally on product pages.
+	wp_register_script( 'iepa-slick-js', plugins_url( '/dist/js/slick.min.js', dirname( __FILE__ ) ), array( 'jquery' ), IEPA_VERSION, false );
+	wp_register_script( 'iepa-fancybox-js', plugins_url( '/dist/js/jquery.fancybox.js', dirname( __FILE__ ) ), array( 'jquery' ), IEPA_VERSION, true );
+	wp_register_script( 'iepa-zoom-js', plugins_url( '/dist/js/jquery.zoom.min.js', dirname( __FILE__ ) ), array( 'jquery' ), IEPA_VERSION, true );
+	wp_register_style( 'iepa-fancybox-css', plugins_url( '/dist/css/fancybox.css', dirname( __FILE__ ) ), array(), IEPA_VERSION );
+	wp_register_style( 'iepa-front-css', plugins_url( '/dist/css/gallery-slider.css', dirname( __FILE__ ) ), array( 'dashicons' ), IEPA_VERSION );
+	wp_register_script( 'iepa-front-js', plugins_url( '/dist/js/gallery-slider.js', dirname( __FILE__ ) ), array( 'jquery' ), IEPA_VERSION, true );
 
 	// WP Localized globals. Use dynamic PHP stuff in JavaScript via `iepaGlobal` object.
 	wp_localize_script(
@@ -98,5 +99,109 @@ function ibtana_ecommerce_product_addons_iepa_block_assets() { // phpcs:ignore
 	);
 }
 
+/**
+ * Determines whether IEPA front-end assets should be loaded.
+ *
+ * @return bool
+ */
+function ibtana_ecommerce_product_addons_should_enqueue_front_assets() {
+	if ( is_admin() || ! is_singular( 'product' ) ) {
+		return false;
+	}
+
+	$product_id = get_queried_object_id();
+
+	if ( $product_id && function_exists( 'has_block' ) && has_block( 'iepa/block-iepa', $product_id ) ) {
+		return true;
+	}
+
+	if ( $product_id && function_exists( 'parse_blocks' ) ) {
+		$product_post = get_post( $product_id );
+
+		if ( $product_post instanceof WP_Post && ! empty( $product_post->post_content ) ) {
+			$blocks = parse_blocks( $product_post->post_content );
+
+			if ( ibtana_ecommerce_product_addons_has_block_recursive( $blocks, 'iepa/block-iepa' ) ) {
+				return true;
+			}
+		}
+	}
+
+	if ( class_exists( 'IEPA_Blocks' ) && method_exists( 'IEPA_Blocks', 'enabled' ) ) {
+		return IEPA_Blocks::enabled( $product_id );
+	}
+
+	return false;
+}
+
+/**
+ * Recursively checks parsed block arrays for a specific block name.
+ *
+ * Supports nested inner blocks and reusable/synced block references.
+ *
+ * @param array  $blocks      Parsed block array from parse_blocks().
+ * @param string $target_name Target block name.
+ * @param array  $visited_ref Reusable block refs already traversed.
+ * @return bool
+ */
+function ibtana_ecommerce_product_addons_has_block_recursive( $blocks, $target_name, &$visited_ref = array() ) {
+	if ( empty( $blocks ) || empty( $target_name ) ) {
+		return false;
+	}
+
+	foreach ( $blocks as $block ) {
+		if ( ! is_array( $block ) ) {
+			continue;
+		}
+
+		if ( isset( $block['blockName'] ) && $target_name === $block['blockName'] ) {
+			return true;
+		}
+
+		if (
+			isset( $block['blockName'], $block['attrs']['ref'] ) &&
+			'core/block' === $block['blockName']
+		) {
+			$ref_id = absint( $block['attrs']['ref'] );
+
+			if ( $ref_id && ! isset( $visited_ref[ $ref_id ] ) ) {
+				$visited_ref[ $ref_id ] = true;
+				$reusable_post = get_post( $ref_id );
+
+				if ( $reusable_post instanceof WP_Post && ! empty( $reusable_post->post_content ) ) {
+					$reusable_blocks = parse_blocks( $reusable_post->post_content );
+
+					if ( ibtana_ecommerce_product_addons_has_block_recursive( $reusable_blocks, $target_name, $visited_ref ) ) {
+						return true;
+					}
+				}
+			}
+		}
+
+		if ( ! empty( $block['innerBlocks'] ) && ibtana_ecommerce_product_addons_has_block_recursive( $block['innerBlocks'], $target_name, $visited_ref ) ) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+/**
+ * Enqueue IEPA front-end assets only where needed.
+ */
+function ibtana_ecommerce_product_addons_enqueue_front_assets() {
+	if ( ! ibtana_ecommerce_product_addons_should_enqueue_front_assets() ) {
+		return;
+	}
+
+	wp_enqueue_script( 'iepa-slick-js' );
+	wp_enqueue_script( 'iepa-fancybox-js' );
+	wp_enqueue_script( 'iepa-zoom-js' );
+	wp_enqueue_style( 'iepa-fancybox-css' );
+	wp_enqueue_style( 'iepa-front-css' );
+	wp_enqueue_script( 'iepa-front-js' );
+}
+
 // Hook: Block assets.
 add_action( 'init', 'ibtana_ecommerce_product_addons_iepa_block_assets' );
+add_action( 'wp_enqueue_scripts', 'ibtana_ecommerce_product_addons_enqueue_front_assets' );
